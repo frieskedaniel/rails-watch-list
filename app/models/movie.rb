a34f40e0bc4cfea6561a384e52396895cc649ea7
Daniel Frieske <frieskedaniel@gmail.com>
2021-04-14 18:30:05 +0200

    done with rakes, ready to go for rest

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,5 @@
 class Movie < ApplicationRecord
     has_many :bookmarks
     validates :title, presence: true, uniqueness: true
-    before_destroy :prevent_destroy
     validates :overview, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_14_155218) do
+ActiveRecord::Schema.define(version: 2021_04_14_160544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,9 @@ ActiveRecord::Schema.define(version: 2021_04_14_155218) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "list_id"
+    t.bigint "movie_id", null: false
     t.index ["list_id"], name: "index_bookmarks_on_list_id"
+    t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
   end
 
   create_table "lists", force: :cascade do |t|
@@ -38,4 +40,5 @@ ActiveRecord::Schema.define(version: 2021_04_14_155218) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "bookmarks", "movies"
 end
