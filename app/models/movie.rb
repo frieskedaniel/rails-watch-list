4ad7ee96bc45e83d9ed597d63b5af6004035b0ad
Daniel Frieske <frieskedaniel@gmail.com>
2021-04-14 13:02:54 +0200

    started

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+    has_many :bookmarks
+    validates :title, presence: true, uniqueness: true
+    before_destroy :prevent_destroy
+    validates :overview, presence: true
 end
--- db/migrate/20210414091344_create_movies.rb
+++ db/migrate/20210414091344_create_movies.rb
@@ -2,8 +2,8 @@ class CreateMovies < ActiveRecord::Migration[6.1]
   def change
     create_table :movies do |t|
       t.string :title
-      t.text :overview
-      t.url :poster_url
+      t.string :overview
+      t.string :poster_url
       t.float :rating
 
       t.timestamps
