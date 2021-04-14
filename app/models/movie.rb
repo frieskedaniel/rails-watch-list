89153e25266b9853fae1d1d7d13ce2bd52c070c7
Daniel Frieske <frieskedaniel@gmail.com>
2021-04-14 13:03:08 +0200

    started

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
-    has_many :bookmarks
+    has_many :bookmarks, depe
     validates :title, presence: true, uniqueness: true
     before_destroy :prevent_destroy
     validates :overview, presence: true
